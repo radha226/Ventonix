29ce979c8866965330021480609388ca752ccb0a
radha226 <radhasingh226@gmail.com>
2021-02-22 01:39:18 +0530

    feat: about, carrers, job listing , application pages complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -78,36 +78,36 @@ export class HomeComponent implements OnInit {
       //     });
     }, 1000);
 
-    var counted = 0;
-    $(window).scroll(function () {
-      console.log("yess scrolling");
-      var oTop = $("#counter").offset().top - window.innerHeight;
-      if (counted == 0 && $(window).scrollTop() > oTop) {
-        $(".count").each(function () {
-          var $this = $(this),
-            countTo = $this.attr("data-count");
-          $({
-            countNum: $this.text(),
-          }).animate(
-            {
-              countNum: countTo,
-            },
+    // var counted = 0;
+    // $(window).scroll(function () {
+    //   console.log("yess scrolling");
+    //   var oTop = $("#counter").offset().top - window.innerHeight;
+    //   if (counted == 0 && $(window).scrollTop() > oTop) {
+    //     $(".count").each(function () {
+    //       var $this = $(this),
+    //         countTo = $this.attr("data-count");
+    //       $({
+    //         countNum: $this.text(),
+    //       }).animate(
+    //         {
+    //           countNum: countTo,
+    //         },
 
-            {
-              duration: 2000,
-              easing: "swing",
-              step: function () {
-                $this.text(Math.floor(this.countNum));
-              },
-              complete: function () {
-                $this.text(this.countNum);
-                //alert('finished');
-              },
-            }
-          );
-        });
-        counted = 1;
-      }
-    });
+    //         {
+    //           duration: 2000,
+    //           easing: "swing",
+    //           step: function () {
+    //             $this.text(Math.floor(this.countNum));
+    //           },
+    //           complete: function () {
+    //             $this.text(this.countNum);
+    //             //alert('finished');
+    //           },
+    //         }
+    //       );
+    //     });
+    //     counted = 1;
+    //   }
+    // });
   }
 }
