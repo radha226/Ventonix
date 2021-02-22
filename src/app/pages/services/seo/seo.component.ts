ec4932eb34667a2ba46bd9642d754a7820dfe6bf
radha226 <radhasingh226@gmail.com>
2021-02-23 01:25:12 +0530

    feat : seo page

--- src/app/pages/services/seo/seo.component.ts
+++ src/app/pages/services/seo/seo.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-seo',
-  templateUrl: './seo.component.html',
-  styleUrls: ['./seo.component.css']
+  selector: "app-seo",
+  templateUrl: "./seo.component.html",
+  styleUrls: ["./seo.component.scss"],
 })
 export class SeoComponent implements OnInit {
+  data = {
+    title: "Happy with what we offer? Grab it Now!",
+    desc:
+      "Yeah ! Its pretty cool about what you are offering and i starterd to feel in love with your Amazing UI Kit",
+    link: "/contact",
+    linkText: "Ask a quotation",
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
