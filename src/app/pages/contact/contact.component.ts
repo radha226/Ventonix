bc0fb22d50c6da2a10e378157484dda2b3ae1f5f
radha226 <radhasingh226@gmail.com>
2021-02-25 00:51:00 +0530

    feat: contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  contactUs: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.checkContactformValidation();
   }
-
+  checkContactformValidation() {
+    this.contactUs = this.fb.group({
+      fullName: ["", Validators.required],
+      phone: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      message: ["", Validators.required],
+      conditon: [true],
+    });
+  }
+  onSubmit(data) {
+    console.log(data.value);
+    if (!data.invalid) {
+      this.checkContactformValidation;
+    } else {
+      console.log(data.value);
+    }
+  }
+  formName() {
+    return this.contactUs.controls;
+  }
 }
