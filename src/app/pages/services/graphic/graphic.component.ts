7a04e8175b06ccd7a1fbb3b2d434ecc8693e5123
radha226 <radhasingh226@gmail.com>
2021-02-24 22:58:56 +0530

    feat: add graphic page

--- src/app/pages/services/graphic/graphic.component.ts
+++ src/app/pages/services/graphic/graphic.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-graphic',
-  templateUrl: './graphic.component.html',
-  styleUrls: ['./graphic.component.css']
+  selector: "app-graphic",
+  templateUrl: "./graphic.component.html",
+  styleUrls: ["./graphic.component.scss"],
 })
 export class GraphicComponent implements OnInit {
+  data = {
+    title: "Are you happy with what we offer? Grab your wrapkit copy now",
+    desc:
+      "You can relay on our amazing features list and also our customer services will be great experience for you without doubt and in no-time",
+    link: "/contact",
+    linkText: "Ask for quotation",
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
